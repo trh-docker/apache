dd2c2aebee76ef0e25a0fc430b74d5fe58d81e63
SpivEgin <spivegin@txtsme.com>
2019-03-08 09:48:02 -0500

    changed php last

--- files/bash/entry.sh
+++ files/bash/entry.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/dumb-init /bin/sh
 
-# Run PHP in background
-/usr/sbin/php-fpm7.0 --nodaemonize --fpm-config /etc/php/7.0/fpm/php-fpm.conf &
 # Apache gets grumpy about PID files pre-existing
 rm -f /usr/local/apache2/logs/httpd.pid
 chmod +x /etc/apache2/envvars 
 sh /etc/apache2/envvars
-/usr/sbin/apache2 -DFOREGROUND
+/usr/sbin/apache2 &
+
+# Run PHP in background
+/usr/sbin/php-fpm7.0 --nodaemonize --fpm-config /etc/php/7.0/fpm/php-fpm.conf 
