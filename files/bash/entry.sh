c79cd2f52489e1ecd005972638310968c9ebf926
SpivEgin <spivegin@txtsme.com>
2019-03-07 21:47:51 -0500

    try again

--- files/bash/entry.sh
+++ files/bash/entry.sh
@@ -2,9 +2,6 @@
 
 # Run PHP in background
 /usr/sbin/php-fpm7.0 --nodaemonize --fpm-config /etc/php/7.0/fpm/php-fpm.conf &
-set -e
-
 # Apache gets grumpy about PID files pre-existing
 rm -f /usr/local/apache2/logs/httpd.pid
-
-exec httpd -DFOREGROUND
+httpd -DFOREGROUND
