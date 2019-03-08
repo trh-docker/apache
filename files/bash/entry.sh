80e76195bfae9702bb46d12b4b9da4970fc4075b
SpivEgin <spivegin@txtsme.com>
2019-03-08 08:21:25 -0500

    small update

--- files/bash/entry.sh
+++ files/bash/entry.sh
@@ -4,4 +4,6 @@
 /usr/sbin/php-fpm7.0 --nodaemonize --fpm-config /etc/php/7.0/fpm/php-fpm.conf &
 # Apache gets grumpy about PID files pre-existing
 rm -f /usr/local/apache2/logs/httpd.pid
+chmod +x /etc/apache2/envvars 
+./etc/apache2/envvars
 /usr/sbin/apache2
